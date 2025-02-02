6dc09b41b4e76441e260d753d40630c4b33c211c
Gishman-creator <147145228+Gishman-creator@users.noreply.github.com>
2025-02-02 22:20:13 +0200

    Finished making the Landing page side of the frontend

---
+++ frontend/app/(landingPage)/_components/Nav.tsx
@@ -0,0 +1,39 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React, { ComponentProps } from 'react'
+
+const Nav = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <nav className='bg-white flex items-center justify-between font-dm-sans py-3 px-14 fixed top-0 left-0 right-0 z-10'>
+            <div className='flex items-center justify-center font-bold'>
+                <Image
+                    src="/favicon.svg"
+                    alt="Umurava logo"
+                    width={50}
+                    height={5}
+                    priority
+                />
+                <p className='text-base hover:text-primary'>Umurava</p>
+            </div>
+            <div className='flex items-center justify-between space-x-10'>
+                {children}
+            </div>
+            <Button className='bg-secondary text-white text-xs'>Join the Program</Button>
+        </nav>
+    )
+}
+
+const NavItem = (props: Omit<ComponentProps<typeof Link>, "className">) => {
+    const pathName = usePathname();
+    return (
+        <Link {...props} className={cn('flex items-center text-sm hover:text-primary',
+            pathName == props.href && 'text-primary'
+        )} />
+    )
+}
+
+export { Nav, NavItem }
---
+++ frontend/app/(landingPage)/challenges/page.tsx
@@ -0,0 +1,25 @@
+import { MoveLeft } from 'lucide-react'
+import React from 'react'
+import challengeData from "@/data/challengeData.json";
+import ChallengeCard from '../_components/ChallengeCard';
+import { GoBackButton } from '@/components/Components';
+
+function page() {
+    return (
+        <div className='bg-[#F9FAFB] pt-28 pb-16 px-14'>
+            <div className='flex items-center gap-2'>
+                <GoBackButton />
+                <p className='text-gray-600'>Go Back</p>
+                <p className='text-primary '>/ Challenges & Hackathons</p>
+            </div>
+
+            <div className="grid grid-cols-4 justify-between gap-6 mt-10">
+                {challengeData.slice(0, 12).map((challenge) => (
+                    <ChallengeCard key={challenge.id} challenge={challenge} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default page
