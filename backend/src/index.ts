7e52edcbc50c58db5c80db60a856c8b90dc9ef2b
andre byukusenge <andrewbyukusenge4@gmail.com>
2025-02-11 08:20:57 +0200

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ import path from 'path';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT; 
+const port = process.env.PORT || 3000; 
 
 // CORS configuration
 const corsOptions = {
